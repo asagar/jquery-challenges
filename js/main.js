ace5afc8020188f4f509b73b1dad6eff0036eb21
asagar <asagar@albany.edu>
2014-11-25 18:54:10 -0500

    edit

--- js/main.js
+++ js/main.js
@@ -4,13 +4,8 @@ $('btn-menu').click(function(){
 });
 
 
-/*
 
-## 2. Create accordion panels
-1. On browser load, the content for each panel is hidden.
-2. When you click a panel title, *that* content appears.
-3. When you click another panel title, *that* content appears, but the content of any other opened panel disappears.
-4. Bonus: when you click an already open panel's title, it closes.
-*/
+
+//## 2. Create accordion panels
 
 $('.panel-content').hide();
