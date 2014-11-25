de8800ee452600e2267cf5ca162641e63c1117c2
asagar <asagar@albany.edu>
2014-11-25 18:52:51 -0500

    number2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+//## 1 moble app
 $('btn-menu').click(function(){
    $('.nav-main').slideDown;
 });
+
+
+/*
+
+## 2. Create accordion panels
+1. On browser load, the content for each panel is hidden.
+2. When you click a panel title, *that* content appears.
+3. When you click another panel title, *that* content appears, but the content of any other opened panel disappears.
+4. Bonus: when you click an already open panel's title, it closes.
+*/
+
+$('.panel-content').hide();
